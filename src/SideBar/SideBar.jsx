fec16c03605d09decb1f76491ee7faa1a017aa6f
Deliay <admin@remiliascarlet.com>
2018-03-09 12:40:25 +0800

    勾选状态直接映射渲染状态

--- src/SideBar/SideBar.jsx
+++ src/SideBar/SideBar.jsx
@@ -11,6 +11,8 @@ export class SideBar extends Component {
             status: 0,
             data: [],
         };
+        this.onHeaderClick = this.onHeaderClick.bind(this);
+        this.onItemClick = this.onItemClick.bind(this);
     }
 
     componentDidMount() {
@@ -26,12 +28,36 @@ export class SideBar extends Component {
             this.setState(JSON.parse(req.responseText));
         };
         req.send();
+    }
 
+    /**
+     * Handle header change
+     * @param {Number} index 
+     * @param {React.ChangeEvent<HTMLInputElement>} event 
+     */
+    onHeaderClick(index, event) {
+        let currentState = this.state;
+        for (let subIndex = 0; subIndex < currentState.data[index].jobs.length; subIndex++) {
+            currentState.data[index].jobs[subIndex].checked = event.target.checked;
+        }
+        this.setState(currentState);
+    }
+
+    onItemClick(header, index, event) {
+        let currentState = this.state;
+        currentState.data[header].jobs[index].checked = event.target.checked;
+        this.setState(currentState);
     }
 
     render() {
-        const deptsList = this.state.data.map(p => 
-            <BarList data={p} key={p.deptName} />
+        //从数据中生成父级list，并将数据移交父级list处理
+        const deptsList = this.state.data.map((p, i) => 
+            <BarList 
+                data={p} 
+                onHeaderClick={this.onHeaderClick}
+                onItemClick={this.onItemClick}
+                key={p.deptName}
+                index={i} />
         );
         return (
             <div className="sider-bar">
